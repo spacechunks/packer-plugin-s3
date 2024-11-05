a61e59f39655befa404af4a3082fad87c807f1be
yannic rieger <yannic.rieger@hetzner-cloud.de>
2024-11-05 14:39:56 +0100

    bump plugin version

--- main.go
+++ main.go
@@ -11,7 +11,7 @@ import (
 func main() {
 	ps := plugin.NewSet()
 	ps.RegisterProvisioner(plugin.DEFAULT_NAME, &S3Provisioner{})
-	ps.SetVersion(version.NewPluginVersion("1.0.0", "", ""))
+	ps.SetVersion(version.NewPluginVersion("2.0.0", "", ""))
 	err := ps.Run()
 	if err != nil {
 		fmt.Fprintln(os.Stderr, err.Error())
