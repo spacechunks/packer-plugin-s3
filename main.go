2987cc7d25b5b9a9bff35089928fb53b53400e14
yannic rieger <ybr@76k.io>
2025-04-05 15:27:01 +0200

    bump version to 2.0.3

--- main.go
+++ main.go
@@ -11,7 +11,7 @@ import (
 func main() {
 	ps := plugin.NewSet()
 	ps.RegisterProvisioner(plugin.DEFAULT_NAME, &S3Provisioner{})
-	ps.SetVersion(version.NewPluginVersion("2.0.2", "", ""))
+	ps.SetVersion(version.NewPluginVersion("2.0.3", "", ""))
 	err := ps.Run()
 	if err != nil {
 		fmt.Fprintln(os.Stderr, err.Error())
